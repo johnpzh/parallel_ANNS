71bf812a889b6a676fb26dac3cd9d156dff8c1ba
Zhen Peng <hi.pengzhen@gmail.com>
2020-10-18 11:24:51 -0400

    for SciClone

--- scripts/test47.simple_v3_large_graph_ranged_L.py
+++ scripts/test47.simple_v3_large_graph_ranged_L.py
@@ -23,11 +23,33 @@ env_vars = os.environ
 env_vars["KMP_AFFINITY"] = "granularity=fine,compact,1,0"
 bin="./profile_para_single_query_search_simple_v3_large_graph_ranged"
 
+#
+# #### DEEP100M
+# data_dir = base_dir + "/deep1b"
+# data_name = "deep100M"
+# label = F"{tag}.deep100M"
+# raw_file = F"output.{label}.raw.txt"
+#
+# subprocess.run(F':> {raw_file}', shell=True, check=True)
+# L_min=L_lower
+# L_max=L_upper
+# command = F"{bin} {data_dir}/{data_name}_base.fvecs {data_dir}/{data_name}_query.fvecs {data_dir}/{data_name}.nsg " \
+#           F"{L_min} 100 output.ivecs {data_dir}/{data_name}.true-100_NN.v2.binary " \
+#           F"{num_t} {L_max} {L_step} {X_low} {X_step}" \
+#           F"| tee -a {raw_file}"
+# subprocess.run(command, env=env_vars, shell=True, check=True)
+#
+# rows_file = F"output.{label}.rows.txt"
+# table_file = F"output.{label}.table.txt"
+# selected_file = F"output.{label}.table.selected.txt"
+# subprocess.run(F'python3 ../scripts/output_rows_to_table.py {raw_file} {rows_file} 2 3 10 9 12 13 15 1', shell=True, check=True)
+# subprocess.run(F'python3 ../scripts/output_row_minimum.py {rows_file} {table_file} 2 0', shell=True, check=True)
+# subprocess.run(F'python3 ../scripts/output_find_runtime_above_presicion.py {table_file} {selected_file} 0 2', shell=True, check=True)
 
-#### DEEP100M
-data_dir = base_dir + "/deep1b"
-data_name = "deep100M"
-label = F"{tag}.deep100M"
+#### SIFT100M
+data_dir = base_dir + "/sift1b"
+data_name = "sift100M"
+label = F"{tag}.sift100M"
 raw_file = F"output.{label}.raw.txt"
 
 subprocess.run(F':> {raw_file}', shell=True, check=True)
