959f479879369c108e7c75904c67a5dc05f6c23d
Zhen Peng <hi.pengzhen@gmail.com>
2020-10-21 13:25:04 -0400

    for Skylake

--- scripts/skylake/sh4.PSS_deep100M_T4.sh
+++ scripts/skylake/sh4.PSS_deep100M_T4.sh
@@ -4,10 +4,10 @@ bin="python3 ../scripts/skylake/test47.simple_v3_large_graph_ranged_L.py"
 data_path="/scratch/zpeng"
 
 set -x
-${bin} ${data_path} SV3_T4_L90-100 4 90 100 2 1 2
-${bin} ${data_path} SV3_T4_L100-400 4 100 400 32 2 64
-${bin} ${data_path} SV3_T4_L400-800 4 400 800 64 2 128
-${bin} ${data_path} SV3_T4_L800-1600 4 800 1600 128 2 256
+eval ${bin} ${data_path} SV3_T4_L90-100 4 90 100 2 1 2
+eval ${bin} ${data_path} SV3_T4_L100-400 4 100 400 32 2 64
+eval ${bin} ${data_path} SV3_T4_L400-800 4 400 800 64 2 128
+eval ${bin} ${data_path} SV3_T4_L800-1600 4 800 1600 128 2 256
 #${bin} ${data_path} SV3_T4_L1600-3200 4 1600 3200 64 2 128
 #${bin} ${data_path} SV3_T4_L3200-5000 4 3200 5000 128 2 256
 
