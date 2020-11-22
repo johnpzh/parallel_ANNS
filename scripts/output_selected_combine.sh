f36150d1e894e7c8bf52fac25f6ee71d1895b94e
Zhen Peng <hi.pengzhen@gmail.com>
2020-11-22 13:36:00 -0500

    for Skylake

--- scripts/output_selected_combine.sh
+++ scripts/output_selected_combine.sh
@@ -1,5 +1,5 @@
 
 for num_t in 2 4 8; do
-    cat output.PSS_T1_P0.9*.table.txt > table_T${num_t}.txt
+    cat output.PSS_T${num_t}_P0.9*.table.txt > table_T${num_t}.txt
     python3 ../../scripts/output_find_runtime_above_presicion.py table_T${num_t}.txt selected_T${num_t}.txt 0 2
 done
